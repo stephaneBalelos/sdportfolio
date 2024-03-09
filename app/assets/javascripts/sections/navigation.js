076d85ed3ea3bcf94828af446843b02053ce21d6
Stephane <sd@intelligentmobiles.com>
2024-03-09 16:01:43 +0100

    Nav brand animation

--- app/assets/javascripts/sections/navigation.js
+++ app/assets/javascripts/sections/navigation.js
@@ -1,7 +1,42 @@
+import gsap from "gsap"
+
 const Section = {
 
-  // load: (section) => {
-  // },
+  load: (section) => {
+    const brand = section.querySelector('.nav-brand')
+    const extraNames = brand.querySelectorAll('[extra-name]')
+    const cursor = brand.querySelector('[cursor]')
+    const tl = gsap.timeline({
+      onComplete: function () {
+      },
+      onReverseComplete: () => {
+        brand.appendChild(cursor)
+      }
+    })
+    Array.from(extraNames).forEach((name, index) => {
+      name.innerHTML = name.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+      const letters = name.querySelectorAll('.letter')
+      let iteration = 0
+      tl.set(letters, {
+        display: 'inline-block',
+        stagger: .05,
+        onUpdate: () => {
+          const l = Array.from(letters)[iteration]
+          name.insertBefore(cursor, l)
+          iteration++
+        }
+      })
+    })
+    tl.paused(true)
+
+    brand.addEventListener('mouseenter', () => {
+      tl.play()
+    })
+    brand.addEventListener('mouseleave', () => {
+      tl.reversed(true)
+    })
+
+},
 
   // unload: (section) => {
   // },
