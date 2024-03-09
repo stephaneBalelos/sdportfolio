e8a640486d0f47e2bfaff81bdf784f6227966e46
Stephane <sd@intelligentmobiles.com>
2024-03-09 13:55:17 +0100

    Contact Form

--- app/assets/javascripts/sections/index.js
+++ app/assets/javascripts/sections/index.js
@@ -8,3 +8,4 @@ export { default as Navigation } from './navigation';
 export { default as HeroHeader } from './hero_header';
 export { default as About } from './about';
 export { default as Work } from './work';
+export { default as Contact } from './contact';
