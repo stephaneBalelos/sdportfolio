7b0f3338d066739371d4270f834cbcd1d99de53b
Stephane <sd@intelligentmobiles.com>
2024-03-09 15:11:24 +0100

    hero header animation

--- app/assets/javascripts/sections/hero_header.js
+++ app/assets/javascripts/sections/hero_header.js
@@ -1,17 +1,44 @@
 import gsap from "gsap";
 
+const letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+
 const Section = {
 
   load: (section) => {
+    let interval = null;
+
     const headerTitle = document.querySelector('[hero-header]')
     if (headerTitle) {
-      headerTitle.innerHTML = headerTitle.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+      // headerTitle.innerHTML = headerTitle.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
       const tl = gsap.timeline({})
-      tl.to(headerTitle.querySelectorAll('.letter'), {
-        opacity: 1, ease: "expo.out",
-        duration: .6,
-        stagger: .034
-      })
+      // tl.to(headerTitle.querySelectorAll('.letter'), {
+      //   opacity: 1, ease: "expo.out",
+      //   duration: .6,
+      //   stagger: .034
+      // })
+      let iteration = 0;
+  
+      clearInterval(interval);
+      
+      interval = setInterval(() => {
+        headerTitle.innerText = headerTitle.innerText
+          .split("")
+          .map((letter, index) => {
+            if(index < iteration) {
+              return headerTitle.dataset.value[index];
+            }
+          
+            return letters[Math.floor(Math.random() * 52)]
+          })
+          .join("");
+        
+        if(iteration >= headerTitle.dataset.value.length){ 
+          clearInterval(interval);
+        }
+        
+        iteration += 1 ;
+      }, 30);
     }
   },
 
