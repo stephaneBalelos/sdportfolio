644a02c3ad42eb37fe2d2d4532185e91ba5a5a10
Stephane <sd@intelligentmobiles.com>
2024-06-05 19:05:45 +0200

    Contact title scroll animation

--- app/assets/javascripts/sections/contact.js
+++ app/assets/javascripts/sections/contact.js
@@ -1,7 +1,27 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger)
+
 const Section = {
 
-  // load: (section) => {
-  // },
+  load: (section) => {
+    const title = section.querySelector('.contact-title')
+    title.innerHTML = title.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: title,
+        start: 'top 80%',
+        end: 'center center',
+        scrub: 3,
+        toggleActions: "play pause resume reset"
+      },
+      
+    })
+
+    tl.to(title.querySelectorAll('.letter'), {
+      backgroundPosition: '0% 0%', ease: 'expo.out', stagger: .5,
+    })
+  },
 
   // unload: (section) => {
   // },
