3f744c1dc000f456279ef7ebd7ced9ff295da9e4
Stephane <sd@intelligentmobiles.com>
2024-06-11 14:30:28 +0200

    Work item shuffle

--- app/assets/javascripts/sections/work.js
+++ app/assets/javascripts/sections/work.js
@@ -1,7 +1,58 @@
+import gsap from "gsap";
+import utils from "../utils";
+
 const Section = {
 
-  // load: (section) => {
-  // },
+  load: (section) => {
+    const titlesToShuffle = [...section.querySelectorAll('[text-shuffle-alternate]')]
+    const workItem = [...section.querySelectorAll('.work-item')]
+    workItem.forEach(item => {
+      const title = item.querySelector('[text-shuffle-alternate]');
+      const initialText = title.dataset.initial;
+      const alternateText = title.dataset.alternate;
+
+      item.addEventListener('mouseenter', () => {
+        shuffleChars(title, alternateText)
+      })
+      item.addEventListener('mouseleave', () => {
+        shuffleChars(title, initialText)
+      })
+    })
+
+    
+
+    function shuffleChars(title, to) {
+      console.log(to)
+      title.innerHTML = to
+      title.innerHTML = title.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter' data-initial='$&'>$&</span>");
+      const chars = [...title.querySelectorAll('.letter')];
+      // const repetitions = arr.length > to.length ? arr.length : to.length;
+      chars.forEach((char, position) => {
+        let iterations = 0;
+          gsap.killTweensOf(char);
+          gsap.fromTo(char, {
+              opacity: 1
+          }, {
+              duration: 0.03,
+              innerHTML: () => {
+                // iterations++;
+                console.log(char.dataset.initial)
+                if(position < iterations) {
+                  return char.dataset.initial ? char.dataset.initial : " ";
+                } else {
+                  return utils.lettersAndSymbols[Math.floor(Math.random() * utils.lettersAndSymbols.length)];
+                }
+              },
+              repeat: to.length,
+              repeatRefresh: true,
+              opacity: 1,
+              onRepeat: () => iterations++,
+              onComplete: () => gsap.set(char, {innerHTML: char.dataset.initial ? char.dataset.initial : " ", delay: 0.03}),
+          })
+      });
+    }
+
+  },
 
   // unload: (section) => {
   // },
